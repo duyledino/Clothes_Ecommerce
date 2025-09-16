6d7c99c2786a880cd48fdabff20d8a3799d9e49f
MyHP <lekhanhduy.bh@gmail.com>
2025-09-16 15:03:16 +0700

    update project

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-    domains: ['res.cloudinary.com','yisol-idm-vton.hf.space'],
+  images: {
+    domains: ["res.cloudinary.com", "yisol-idm-vton.hf.space"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // ESLint will not run during build
   },
 };
 
