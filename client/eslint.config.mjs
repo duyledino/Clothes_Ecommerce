5a1751338ed3e456ed66891cd32c1419e9c9c878
MyHP <lekhanhduy.bh@gmail.com>
2025-09-16 14:59:47 +0700

    update project

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -11,14 +11,14 @@ const compat = new FlatCompat({
 
 // Base config
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
-  {
-    // 👇 add overrides
-    files: ["app/**/page.tsx"],
+  ...compat.config({
+    extends: ["next"],
     rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
       "react-hooks/rules-of-hooks": "off", // disable for Next.js pages
     },
-  },
+  }),
 ];
 
 export default eslintConfig;
