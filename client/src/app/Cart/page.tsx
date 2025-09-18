8985e6e14d396372c3b4aa850e992a0e24f3cd10
MyHP <lekhanhduy.bh@gmail.com>
2025-09-18 22:16:11 +0700

    update project 1016

--- client/src/app/Cart/page.tsx
+++ client/src/app/Cart/page.tsx
@@ -64,6 +64,7 @@ const CartPage = () => {
       toast.error(e);
     }
     if (PaymentURL) {
+      window.location.href = PaymentURL;
       setProcess(true);
     }
   }, [PaymentURL, paymentError, MessageOrder, errorOrder]);
